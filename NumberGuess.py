850f64d7ae43d34b52101c5f50d17702b08ce434
vinilpaulp@gmail.com <vinilpaulp@gmail.com>
2025-07-24 15:16:36 +0400

    add audio,image and game summary

--- NumberGuess.py
+++ NumberGuess.py
@@ -1,24 +1,56 @@
+import base64
 import streamlit as stream
 import random
+import pandas as pd
 if 'generated_number' not in stream.session_state:
     stream.session_state.generated_number=random.randint(1,100)
 if 'chance'not in stream.session_state:
     stream.session_state.chance=0
 if 'guessed' not in stream.session_state:
     stream.session_state.guessed=False
+if 'player_name' not in stream.session_state:
+    stream.session_state.player_name=""
 stream.title("NUMBER GUESS GAME")
-stream.write("Guess a number between 1 and 100")
-guess=stream.number_input("Enter your guess",min_value=1,max_value=100,step=1)
-if stream.button("Submit your Guess"):
-    stream.session_state.chance=stream.session_state.chance+1   
-    if guess==stream.session_state.generated_number:
-        stream.success(f"Congrats!you guessed it right.You won at {stream.session_state.chance} th  chance!")
-        stream.session_state.guessed=True
-    elif guess<stream.session_state.generated_number:
-        stream.warning("your guess is too low!Try again...")
-    else :
-        stream.warning("your guess is too high!Try again...")
-if stream.session_state.guessed:
-    if stream.button("Play Again"):
-        stream.session_state.generated_number=random.randint(1,100) 
-        stream.session_state.guessed=False
+stream.image("logo.png",width=200,)
+player_name=stream.text_input("Enter your name: ")
+stream.button("Let's Go")
+def autoplay_audio(file_path):
+    with open(file_path, "rb") as f:
+        b64 = base64.b64encode(f.read()).decode()
+        stream.markdown(
+        f"<audio autoplay><source src='data:audio/mp3;base64,{b64}' type='audio/mp3'></audio>",
+        unsafe_allow_html=True,
+    )
+if player_name and stream.button:
+    stream.session_state.player_name=player_name
+    stream.write("Guess a number between 1 and 100")
+    guess=stream.number_input("Enter your guess",min_value=1,max_value=100,step=1)
+    if stream.button("Submit your Guess"):
+        stream.session_state.chance=stream.session_state.chance+1   
+        if guess==stream.session_state.generated_number:
+            autoplay_audio("cheering.mp3")
+            stream.success(f"Congrats!you guessed it right.You won at {stream.session_state.chance} th  chance!")
+            stream.session_state.guessed=True
+        elif guess<stream.session_state.generated_number:
+            stream.warning("your guess is too low!Try again...")
+        else :
+            stream.warning("your guess is too high!Try again...")
+    player_details=[{
+        "Number of Attempt":stream.session_state.chance,
+        "win or fail":stream.session_state.guessed,
+        "last guess":guess
+    }]
+    df=pd.DataFrame(player_details)
+    df.to_csv("player_details.csv",mode="w",header=True,index=False)
+    if stream.session_state.guessed==True:
+        if stream.button("summary of your game") :
+            
+            df=pd.read_csv("player_details.csv")
+            stream.dataframe(df)
+    if stream.session_state.guessed:
+        if stream.button("Play Again"):
+            stream.session_state.generated_number=random.randint(1,100) 
+            stream.session_state.guessed=False
+    
+else:
+    stream.warning("Enter your name correctly and press let's Go button!!!")
